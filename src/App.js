ea3d3cf2d013b64431f8ed65c7453e6c3792a734
Vinay Sangwan <73075190+vinay4194@users.noreply.github.com>
2022-04-06 20:24:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
   const [ridesData, setRidesData] = useState()
   const [user, setUser] = useState()
 
+  //This will return the shortest distance of a ride from the user's location 
   const getDistance = (user, ride) => {
     const closest = ride.reduce((a, b) => {
       return Math.abs(b - user) < Math.abs(a - user) ? b : a
@@ -21,8 +22,9 @@ export default function App() {
   useEffect(() => {
     getUserData().then((userdata) => {
       setUser(userdata)
-
+      
       getRidesData().then((data) => {
+        //--This will add a distance element to the array representing the distance from the user's location--
         const rideDistance = data?.data?.map((obj) => ({ ...obj, distance: `${getDistance(userdata?.data?.station_code, obj?.station_path)}` }))
         const sortedRides = rideDistance?.sort((a, b) => {
           return a.distance - b.distance
